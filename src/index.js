94a4e9711b757a5ee82ef1c163f386c88d0851c5
vitthal-07 <vitthalbiradar50@gmail.com>
2025-04-30 11:55:17 +0530

    Fixed cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 // app.js
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js';
-import destinationRoutes from './routes/destinationRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import destinationRoutes from "./routes/destinationRoutes.js";
+import cors from "cors"; // Import CORS middleware
 
 dotenv.config();
 const app = express();
+
+app.use(cors({ origin: "*" })); // Allow all origins for CORS
 
 // Connect to Database
 connectDB();
@@ -15,8 +18,8 @@ connectDB();
 app.use(express.json());
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/destinations', destinationRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/destinations", destinationRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000;
